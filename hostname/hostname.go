007b77de18817d5bfd01e1f915b7aa78860ed250
kthieler <Only4github>
2016-04-20 22:05:33 -0400

    Initial commit

---
+++ hostname/hostname.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+
+	"os"
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hostname)
+	http.ListenAndServe(":8000", nil)
+}
+
+
+func hostname(w http.ResponseWriter, r *http.Request) {
+	name, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	io.WriteString(w,"Hostname reported by kernel : "+name)
+}
+
+
+
